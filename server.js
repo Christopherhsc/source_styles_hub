b17b651c039850f12dba3ea10afc516a3ca31331
Christopher Christensen <christopher.hsc2520@gmail.com>
2024-05-12 17:18:11 +0200

    added cors to server.js

--- server.js
+++ server.js
@@ -1,38 +1,43 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
 const cors = require("cors");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-app.use(cors());
+const app = express();
+
+// Configure CORS for your front-end domain and allowed methods
+app.use(cors({
+  origin: 'https://sourcestylehub.com', // Set the allowed origin
+  methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'] // Correctly list HTTP methods
+}));
+
 app.use(bodyParser.json({ limit: "10mb" }));
 
-mongoose.connect(process.env.MONGODB_URI, {});
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on("connected", () => {
   console.log("Connected to MongoDB");
 });
 
+// Basic route to check API status
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.use(cors({
-  origin: ['https://sourcestylehub.com'],
-  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
-}));
-
+// Import routes
 const userRouter = require("./app/routes/users");
 const snippetRouter = require("./app/routes/snippets");
 const searchRouter = require("./app/routes/search");
 
+// Use routes
 app.use("/users", userRouter);
 app.use("/snippets", snippetRouter);
 app.use("/search", searchRouter);
 
 const port = process.env.PORT || 3000;
 
-// Listen using HTTPS server
+// Server start
 app.listen(port, () => {
   console.log(`Server running at ${port}`);
-})
+});
